269e2e35f3abe98c07d32b1dd66631dbd8e33dd8
Marcus Ryan <marcusryan@Marcuss-MacBook-Pro.local>
2024-05-12 05:37:32 +1000

    format

--- src/App.js
+++ src/App.js
@@ -22,11 +22,11 @@ const images = Object.keys(imagesImported).map(image => ({
 }));
 
   return (
-    <div style={{ backgroundColor: 'pink', height: isMobile ? '90vh' : '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+    <div style={{ backgroundColor: 'pink', height: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
       <div style={{
         backgroundColor: '#17171a',
         width: '90%',
-        height: '85%',
+        height: isMobile ? '95%': '85%',
         display: 'flex',
         flexDirection: 'column',
         justifyContent: 'space-between',
@@ -43,13 +43,13 @@ const images = Object.keys(imagesImported).map(image => ({
         backgroundSize: '50px 50px',
         padding: '20px'
       }}>
-        <h1 className="baking-title">Sprinkles</h1>
-        <h4 className="baking-title1" style={{ marginTop: isMobile ? '-8vh' :'-10vh' }}>Custom cakes and cupcakes</h4>
+        <h1 className="baking-title" style={{ marginTop: isMobile ? '1vh': '0'}}>Sprinkles</h1>
+        <h4 className="baking-title1" style={{ marginTop :isMobile ? '-8vh': '-10vh' }}>Custom cakes and cupcakes</h4>
         <div style={{ width: '220px', height: '200px'}}>
         <ImageGallery items={images} additionalClass="roundedImage" />
         </div>
         <h4 className="baking-title1" style={{ marginTop: '20vh'}}>Order Now/Support Me!</h4>
-        <div style={{ display: 'flex', justifyContent: 'space-between', width: '200px' }}>
+        <div style={{ display: 'flex', justifyContent: 'space-between', width: '200px' , marginBottom: isMobile ? '30vh' :'0'}}>
           <a href="mailto:sprinklesbymaioxley@gmail.com" style={{ color: 'pink' }}><FaEnvelope size={40} className="icon" /></a>
           <a href="https://www.facebook.com/SprinklesbyMaiOxley" style={{ color: 'pink' }}><FaFacebook size={40} className="icon" /></a>
           <a href="https://www.instagram.com/sprinklesbymaioxley" style={{ color: 'pink' }}><FaInstagram size={40} className="icon" /></a>

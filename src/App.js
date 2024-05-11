cdc8e6ec883e2889355b772c0a7df86617eb6607
Marcus Ryan <marcusryan@Marcuss-MacBook-Pro.local>
2024-05-12 04:45:13 +1000

    complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
+import { FaEnvelope, FaFacebook, FaInstagram } from 'react-icons/fa';
+
+import ImageGallery from 'react-image-gallery';
+import 'react-image-gallery/styles/css/image-gallery.css';
+
+function importAll(r) {
+  let images = {};
+  r.keys().map((item, index) => { images[item.replace('./', '')] = r(item); });
+  return images;
+}
+
+const imagesImported = importAll(require.context('./', false, /\.(png|jpe?g|svg)$/));
 
 function App() {
+const images = Object.keys(imagesImported).map(image => ({
+  original: imagesImported[image],
+  thumbnail: imagesImported[image],
+  originalClass: 'roundedImage',
+  thumbnailClass: 'roundedImage'
+}));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ backgroundColor: 'pink', height: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+      <div style={{
+        backgroundColor: '#17171a',
+        width: '90%',
+        height: '85%',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        color: 'pink',
+        borderRadius: '20px',
+        backgroundImage: `radial-gradient(circle at 1% 1%, red 1px, transparent 1px),
+                          radial-gradient(circle at 10% 10%, orange 1px, transparent 1px),
+                          radial-gradient(circle at 20% 20%, yellow 1px, transparent 1px),
+                          radial-gradient(circle at 30% 30%, green 1px, transparent 1px),
+                          radial-gradient(circle at 40% 40%, blue 1px, transparent 1px),
+                          radial-gradient(circle at 50% 50%, indigo 1px, transparent 1px),
+                          radial-gradient(circle at 60% 60%, violet 1px, transparent 1px)`,
+        backgroundSize: '50px 50px',
+        padding: '20px'
+      }}>
+        <h1 className="baking-title">Sprinkles</h1>
+        <h4 className="baking-title1" style={{ marginTop: '-10vh' }}>Custom cakes and cupcakes</h4>
+        <div style={{ width: '220px', height: '200px'}}>
+        <ImageGallery items={images} additionalClass="roundedImage" />
+        </div>
+        <h4 className="baking-title1" style={{ marginTop: '20vh'}}>Order Now/Support Me!</h4>
+        <div style={{ display: 'flex', justifyContent: 'space-between', width: '200px' }}>
+          <a href="mailto:sprinklesbymaioxley@gmail.com" style={{ color: 'pink' }}><FaEnvelope size={40} className="icon" /></a>
+          <a href="https://www.facebook.com/SprinklesbyMaiOxley" style={{ color: 'pink' }}><FaFacebook size={40} className="icon" /></a>
+          <a href="https://www.instagram.com/sprinklesbymaioxley" style={{ color: 'pink' }}><FaInstagram size={40} className="icon" /></a>
+        </div>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
